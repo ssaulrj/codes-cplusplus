11d7e1c9c9d5e9c24bb577a2a702fca64b764493
Saúl Juárez <59984185+ssaulrj@users.noreply.github.com>
2020-04-26 17:04:59 -0500

    Create tempf_to_tempc.cpp

---
+++ codeacademy_cpluscplus/pattern/tempf_to_tempc.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+/*Nueva York, EE. UU.
+domingo, 17:00
+Nublado* April 26th, 2020
+47° F*/
+
+int main() {
+  
+  double tempf = 47.0;
+  double tempc;
+  
+  tempc = (tempf-32)/1.8;
+  
+  std::cout << "The temp is " << tempc << " degrees Celsius.";
+  
+  
+  
+}
